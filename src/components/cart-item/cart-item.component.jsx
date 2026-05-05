c11ff1468cc7af158c8906f5d6996743ea1385e9
Mitchell Brown <mitch7553@gmail.com>
2026-05-05 15:17:31 +0800

    Still creating the rest of the app content

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,17 @@
+import './cart-item.styles.scss';
+
+const CartItem = ({cartItem}) => {
+    const { name, quantity } = cartItem;
+    return (
+        <div>
+            <h2>{name}</h2>
+            <span>Quantity: {quantity}</span>
+        </div>
+    )
+}
+
+
+
+
+
+export default CartItem;
--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -4,6 +4,7 @@ import {createContext, useState, useEffect} from 'react';
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {}   
+    cartItems: [],
 
 });
 
@@ -14,6 +15,7 @@ export const CartContext = createContext({
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const value = {isCartOpen, setIsCartOpen};
+
     return(
      <CartContext.Provider value={value}>{children}</CartContext.Provider>      
     )
