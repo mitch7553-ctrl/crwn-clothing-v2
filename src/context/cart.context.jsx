c0153d3998db090b0b0f3761877bfd1b73e59892
Mitchell Brown <mitch7553@gmail.com>
2026-05-05 16:03:17 +0800

    Finished the addToCart feature

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -20,6 +20,7 @@ export const CartContext = createContext({
     setIsCartOpen: () => {},   
     cartItems: [],
     addItemToCart: () => {},
+    cartCount: 0
 });
 
 
@@ -28,11 +29,19 @@ export const CartContext = createContext({
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
     };
-    const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart};
 
+
+    useEffect(() => {
+        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+        setCartCount(newCartCount);
+    }, [cartItems]);
+
+
+    const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount};
 
     return(
      <CartContext.Provider value={value}>{children}</CartContext.Provider>      
