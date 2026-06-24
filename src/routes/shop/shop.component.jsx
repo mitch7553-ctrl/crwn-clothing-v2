f0129097c6859fe96bf06ff3326e2a2482478726
Mitchell Brown <mitch7553@gmail.com>
2026-06-24 12:19:29 +0800

    Created the category preview component will nest route it soon .

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,5 @@
-import { useContext, Fragment } from 'react';
-
-import ProductCard from '../../components/product-card/product-card.component';
-
+import { useContext } from 'react';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
 import { CategoriesContext } from '../../context/categories.context';
 
 import './shop.styles.scss';
@@ -10,19 +8,14 @@ const Shop = () => {
   const {categoriesMap} = useContext(CategoriesContext);
 
   return (
-    //This is a shorthand for fragment
-    <Fragment>
-        {Object.keys(categoriesMap).map(title => (
-          <Fragment key={title}>
-            <h2>{title}</h2>
-              <div className='products-container'>
-                {categoriesMap[title].map((product) => ( 
-                  <ProductCard key={product.id} product={product} />
-                ))}
-              </div>
-          </Fragment>
-        ))}
-    </Fragment>
+   <div className='shop-container'>
+        {Object.keys(categoriesMap).map(title => {
+          const products = categoriesMap[title];
+          return(
+            <CategoryPreview key={title} title={title} products={products} />
+          )
+        })}
+    </div>
   );
 
 };
