fd94f5d470ed3b7eba6da7f5c93a91578506cd97
Mitchell Brown <mitch7553@gmail.com>
2026-06-27 20:42:08 +0800

    Added  the styled compnonents to all scss files .

--- src/components/directory-item/directory-item.component.js
+++ src/components/directory-item/directory-item.component.js
@@ -1,9 +1,13 @@
 import {BackGroundImage, Body, DirectoryItemContainer} from './directory-item.styles';
+import { useNavigate } from 'react-router-dom';
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+   const onNavigateHandler = () => navigate(route);
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackGroundImage
         className='background-image'
         imageUrl={imageUrl}
