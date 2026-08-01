645ba3c92e72f07fad86cd76632238882674828c
Mitchell Brown <mitch7553@gmail.com>
2026-08-01 15:46:30 +0800

    committing changes

--- src/store/store.js
+++ src/store/store.js
@@ -4,27 +4,13 @@ import { rootReducer } from './root-reducer';
 
 
 
+const loggerMiddleWare = 
 
 
-const loggerMiddleware = (store) => (next) => (action) => {
-    if(!action.type) {
-        return next(action);
-    }
-    console.log('type', action.type);
-    console.log('payload: ', action.payLoad)
-    console.log('currentState: ', store.getState());
+const middleWares = [logger];
 
-    const nextState = next(action);
-    console.log('next state: ' , store.getState());
-    return nextState;
-}
+const composeEnhancers = compose(applyMiddleware(...middleWares));
 
-
-const middleWares = [loggerMiddleware];
-
-const composeEnhancers =
-    (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
-export const store = createStore(rootReducer, undefined, composeEnhancers(applyMiddleware(...middleWares)));
+export const store = createStore(rootReducer, undefined, composeEnhancers);
 
 
